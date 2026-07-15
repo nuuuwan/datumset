53e248982aa1c2cccfb058a96d42abe0e6fc3427
nuuuwan <nuuuwan@gmail.com>
2026-07-15 16:50:26 +0530

    Remove debug print statements from test_queries.py for cleaner test output

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -13,22 +13,9 @@ class TestCase(unittest.TestCase):
             'Person+House/2012+2024/Religion',
             'House+Person/2012+2024/Religion',
         ]:
-            print('=' * 32)
-            print(query_str)
-            print('=' * 32)
-            
             mds1 =  LankaData[query_str]
             s1 =mds1.to_str()
-            print(s1)
-            print('-' * 32)
-
             mds2 = MatchedDatumset.from_str(s1)
-            print(mds2)
-            print('-' * 32)
-
-
-            print(mds2.query.query_str)
-            print('-' * 32)
-            
             self.assertEqual(mds2.query, Query(query_str))
-            print('-' * 32)
+            self.assertEqual(mds1, mds2)
+            
