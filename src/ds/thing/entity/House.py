6b2ee91445f9f4d6b3a7ba824963dd6b2864ff38
nuuuwan <nuuuwan@gmail.com>
2026-07-16 10:21:33 +0530

    Remove unused dataclass and constructor from House and Person entities

--- src/ds/thing/entity/House.py
+++ src/ds/thing/entity/House.py
@@ -1,14 +1,5 @@
-from dataclasses import dataclass
-
 from ds.thing.entity.Entity import Entity
 
 
-@dataclass(frozen=True)
 class House(Entity):
-    id: str
-    address: str
-
-    def __init__(self, id: str, address: str):
-        Entity.__init__(self, id)
-        object.__setattr__(self, "id", id)
-        object.__setattr__(self, "address", address)
+    pass
--- src/ds/thing/entity/Person.py
+++ src/ds/thing/entity/Person.py
@@ -1,14 +1,5 @@
-from dataclasses import dataclass
-
 from ds.thing.entity.Entity import Entity
 
 
-@dataclass(frozen=True)
 class Person(Entity):
-    id: str
-    name: str
-
-    def __init__(self, id: str, name: str):
-        Entity.__init__(self, id)
-        object.__setattr__(self, "id", id)
-        object.__setattr__(self, "name", name)
+    pass
