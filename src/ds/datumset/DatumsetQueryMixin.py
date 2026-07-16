89adeb640e4b4e31a1e3ca79a8a2b36d786e2e89
nuuuwan <nuuuwan@gmail.com>
2026-07-16 19:00:52 +0530

    Refactor DatumsetQueryMixin: reorganize methods for clarity and maintainability

--- src/ds/datumset/DatumsetQueryMixin.py
+++ src/ds/datumset/DatumsetQueryMixin.py
@@ -5,27 +5,36 @@ from ds.query.Query import Query
 
 class DatumsetQueryMixin:
 
-    @cached_property
-    def query(self):
-        return self._infer_query()
-
-    def _infer_query(self) -> Query:
+    def _get_entity_class_names(self):
         entity_class_names = []
-        time_values = []
-        concept_labels = []
-
         for datum in self._value:
             if datum.entity_class.__name__ not in entity_class_names:
                 entity_class_names.append(datum.entity_class.__name__)
+        return entity_class_names
+
+    def _get_time_values(self):
+        time_values = []
+        for datum in self._value:
             if datum.time.get_value() not in time_values:
                 time_values.append(datum.time.get_value())
+        return time_values
+
+    def _get_concept_labels(self):
+        concept_labels = []
+        for datum in self._value:
             for label in datum.get_concept_labels():
                 if label not in concept_labels:
                     concept_labels.append(label)
+        return concept_labels
 
+    def _infer_query(self) -> Query:
         query = Query.from_parts(
-            time_values,
-            entity_class_names,
-            concept_labels,
+            self._get_time_values(),
+            self._get_entity_class_names(),
+            self._get_concept_labels(),
         )
         return query
+
+    @cached_property
+    def query(self):
+        return self._infer_query()
