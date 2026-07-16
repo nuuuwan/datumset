89096da74868cdf19fc1c597520c3cd3325a331f
nuuuwan <nuuuwan@gmail.com>
2026-07-16 09:36:12 +0530

    Refactor test_queries.py to improve readability; replace single quotes with double quotes for consistency and streamline assertions in test_basic method.

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,22 +1,24 @@
 import unittest
 
-from ds import LankaData, MatchedDatumset, Query
+from ds import LankaData, MatchedDatumset
 
 
 class TestCase(unittest.TestCase):
     def test_basic(self):
         for query_str in [
-            'Person/2012/Religion*District',
-            'Person/2024/Religion*District',
-            'Person/2024/District*Religion',
-            'Person/2024/District',
-            'Person/2024/Religion',
-            'Person/2012+2024/Religion',
-            'Person+House/2012+2024/Religion',
-            'House+Person/2012+2024/Religion',
+            "Person/2012/Religion*District",
+            "Person/2024/Religion*District",
+            "Person/2024/District*Religion",
+            "Person/2024/District",
+            "Person/2024/Religion",
+            "Person/2012+2024/Religion",
+            "Person+House/2012+2024/Religion",
+            "House+Person/2012+2024/Religion",
         ]:
             mds1 = LankaData[query_str]
-            s1 = mds1.to_str()
-            mds2 = MatchedDatumset.from_str(s1)
-            self.assertEqual(mds2.query, Query(query_str))
+
+            mds2 = MatchedDatumset.from_str(mds1.to_str())
+            mds3 = MatchedDatumset.from_str(mds2.to_str())
+
             self.assertEqual(mds1, mds2)
+            self.assertEqual(mds1, mds3)
