67c092392b379cf96bc0f46212d1c3e43658ae4a
nuuuwan <nuuuwan@gmail.com>
2026-07-17 10:29:55 +0530

    Add unit tests for Datum matching logic in TestCase

--- tests/test_datum.py
+++ tests/test_datum.py
@@ -38,3 +38,26 @@ class TestCase(unittest.TestCase):
 
         datum2 = Datum.from_data(data)
         self.assertEqual(datum, datum2)
+
+    def test_match(self):
+        datum = Datum(
+            entity_class=ThingFactory["Person"],
+            dim_idx=dict(
+                Time=ThingFactory["Time"]("2012"),
+                District=ThingFactory["District"]["LK-11"],
+                Religion=ThingFactory["Religion"]["Buddhist"],
+            ),
+            cell_idx=dict(
+                Count1=ThingFactory["Int"](123),
+                Count2=ThingFactory["Int"](112),
+            ),
+        )
+
+        self.assertTrue(datum.is_match_entity("Person"))
+        self.assertFalse(datum.is_match_entity("House"))
+
+        self.assertTrue(datum.is_match_dim_idx("Time*District*Religion"))
+        self.assertFalse(datum.is_match_dim_idx("Time*District"))
+
+        self.assertTrue(datum.is_match_cell_idx("Count1*Count2"))
+        self.assertFalse(datum.is_match_cell_idx("Count1"))
