b477950f30da076ae1484dec1f091b3cabeab082
nuuuwan <nuuuwan@gmail.com>
2026-07-17 13:59:33 +0530

    Refactor _get_datum_list_from_d method to simplify logic and improve readability; update get_datumset to use col_map for datum creation

--- src/ds/db/Census2012.py
+++ src/ds/db/Census2012.py
@@ -13,37 +13,25 @@ log = Log("Census2012")
 
 class Census2012:
     TIME = Time("2012")
+    SKIP_KEYS = {"entity_id", "total_population", "region_id"}
 
     @classmethod
-    def _get_datum_list_from_d(cls, d, entity_cls, measurement_cls):
+    def _get_datum_list_from_d(cls, d, entity_cls, col_map):
         region_id = d["entity_id"]
         try:
             region_cls = RegionFactory.from_region_id(region_id)
-        except ValueError:
-            return None
-        region_cls_name = region_cls.__name__
-        try:
             region_instance = region_cls[region_id]
         except ValueError:
             return None
-        measurement_cls_name = measurement_cls.__name__
-        skip = {"entity_id", "total_population", "region_id"}
-
-        datum_list = []
-        for k, v in d.items():
-            if k in skip:
-                continue
-            datum = Datum(
+        r_name = region_cls.__name__
+        return [
+            Datum(
                 entity_cls,
-                {
-                    "Time": cls.TIME,
-                    region_cls_name: region_instance,
-                    measurement_cls_name: measurement_cls[String(k).pascal],
-                },
-                dict(Count=Int(String(v).int)),
+                {"Time": cls.TIME, r_name: region_instance, m_name: concept},
+                {"Count": Int(int(float(d[k].strip().replace(",", ""))))},
             )
-            datum_list.append(datum)
-        return datum_list
+            for k, (m_name, concept) in col_map.items()
+        ]
 
     @classmethod
     def get_datumset(
@@ -68,11 +56,17 @@ class Census2012:
             f"{measurement_id}.{region_group_id}.{year_id}.tsv",
         )
         WWW(url).download(tsv_file)
-        datum_list = []
         d_list = tsv_file.read()
+        m_name = measurement_cls.__name__
+        col_map = {
+            k: (m_name, measurement_cls[String(k).pascal])
+            for k in (d_list[0] if d_list else {})
+            if k not in cls.SKIP_KEYS
+        }
+        datum_list = []
         for d in d_list:
             datum_list_for_d = cls._get_datum_list_from_d(
-                d, entity_cls, measurement_cls
+                d, entity_cls, col_map
             )
             if datum_list_for_d:
                 datum_list.extend(datum_list_for_d)
