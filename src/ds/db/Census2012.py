ff9bc868d3d88bbc2f51b6bd8689e05d87867959
nuuuwan <nuuuwan@gmail.com>
2026-07-17 11:50:54 +0530

    Refactor Census2012: update get_datum_list_from_d to accept entity and measurement classes; enhance list method to include new measurement types; remove obsolete test file.

--- src/ds/db/Census2012.py
+++ src/ds/db/Census2012.py
@@ -2,18 +2,16 @@ from ds.datum.Datum import Datum
 from ds.datumset.Datumset import Datumset
 from ds.thing.concept.Int import Int
 from ds.thing.concept.region.RegionFactory import RegionFactory
-from ds.thing.concept.Religion import Religion
 from ds.thing.concept.Time import Time
-from ds.thing.entity.Person import Person
+from ds.thing.ThingFactory import ThingFactory
 from utils_future import WWW, Directory, String, TSVFile
 
 
 class Census2012:
-    ENTITY_CLASS = Person
     TIME = Time("2012")
 
     @classmethod
-    def get_datum_list_from_d(cls, d):
+    def get_datum_list_from_d(cls, d, entity_cls, measurement_cls):
         region_id = d["entity_id"]
         try:
             region_cls = RegionFactory.from_region_id(region_id)
@@ -24,16 +22,16 @@ class Census2012:
 
         datum_list = []
         for k, v in d.items():
-            if k in ["entity_id", "total_population"]:
+            if k in ["entity_id", "total_population", "region_id"]:
                 continue
             concept_name = String(k).pascal
 
             datum = Datum(
-                cls.ENTITY_CLASS,
+                entity_cls,
                 {
                     "Time": cls.TIME,
-                    f"{region_cls_name}": region_cls[region_id],
-                    "Religion": Religion[concept_name],
+                    region_cls_name: region_cls[region_id],
+                    measurement_cls.__name__: measurement_cls[concept_name],
                 },
                 dict(Count=Int(String(v).int)),
             )
@@ -41,29 +39,61 @@ class Census2012:
         return datum_list
 
     @classmethod
-    def get_religion(cls) -> Datumset:
-        measurement_id = "population-religion"
-        region_id = "regions"
+    def get_datumset(
+        cls,
+        entity_class_name,
+        measurement_class_name,
+        measurement_id,
+        region_group_id,
+    ) -> Datumset:
         year_id = cls.TIME.get_value()
+        entity_cls = ThingFactory[entity_class_name]
+        measurement_cls = ThingFactory[measurement_class_name]
+
         url = (
             "https://raw.githubusercontent.com"
             + "/nuuuwan/gig-data/refs/heads/master"
-            + f"/gig2/{measurement_id}.{region_id}.{year_id}.tsv"
+            + f"/gig2/{measurement_id}.{region_group_id}.{year_id}.tsv"
         )
 
         tsv_file = TSVFile(
             Directory.get_temp("datumset", "census2012").path,
-            f"{measurement_id}.{region_id}.{year_id}.tsv",
+            f"{measurement_id}.{region_group_id}.{year_id}.tsv",
         )
         WWW(url).download(tsv_file)
         datum_list = []
         d_list = tsv_file.read()
         for d in d_list:
-            datum_list_for_d = cls.get_datum_list_from_d(d)
+            datum_list_for_d = cls.get_datum_list_from_d(
+                d, entity_cls, measurement_cls
+            )
             if datum_list_for_d:
                 datum_list.extend(datum_list_for_d)
         return Datumset(*datum_list)
 
     @classmethod
     def list(cls) -> list[Datumset]:
-        return [cls.get_religion()]
+        datumset_list = []
+        for (
+            entity_class_name,
+            measurement_class_name,
+            measurement_id,
+            region_group_id,
+        ) in [
+            ["Person", "Religion", "population-religion", "regions"],
+            [
+                "Person",
+                "IsEconomicallyActive",
+                "economy-economic-activity",
+                "regions",
+            ],
+        ]:
+            datumset = cls.get_datumset(
+                entity_class_name,
+                measurement_class_name,
+                measurement_id,
+                region_group_id,
+            )
+            datumset_list.append(datumset)
+
+        return datumset_list
