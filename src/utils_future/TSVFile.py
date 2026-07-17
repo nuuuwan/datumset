5740c3e6cd6da6074706ffa55eb159760ffc793f
nuuuwan <nuuuwan@gmail.com>
2026-07-17 13:12:25 +0530

    Refactor read method in TSVFile to use csv.reader for improved row handling

--- src/utils_future/TSVFile.py
+++ src/utils_future/TSVFile.py
@@ -9,7 +9,8 @@ log = Log("TSVFile")
 class TSVFile(File):
     def read(self):
         with open(self.path, encoding="utf-8") as f:
-            reader = csv.DictReader(f, delimiter="\t")
-            d_list = list(reader)
+            reader = csv.reader(f, delimiter="\t")
+            headers = next(reader)
+            d_list = [dict(zip(headers, row)) for row in reader if row]
             log.debug(f"Read {len(d_list)} rows from {self}")
             return d_list
