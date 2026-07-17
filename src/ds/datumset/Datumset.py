7265d727ac84ee826d9beddf88d2fcc01500c43c
nuuuwan <nuuuwan@gmail.com>
2026-07-17 14:36:04 +0530

    Remove DatumsetMatchMixin from Datumset class to streamline inheritance

--- src/ds/datumset/Datumset.py
+++ src/ds/datumset/Datumset.py
@@ -1,10 +1,8 @@
 from ds.datumset.DatumsetBase import DatumsetBase
-from ds.datumset.DatumsetMatchMixin import DatumsetMatchMixin
 from ds.datumset.DatumsetSerializeMixin import DatumsetSerializeMixin
 
 
 class Datumset(
-    DatumsetMatchMixin,
     DatumsetSerializeMixin,
     DatumsetBase,
 ):
