7e0d1f13999507ec92c03d11e16998d1860d037b
nuuuwan <nuuuwan@gmail.com>
2026-07-17 11:36:51 +0530

    Remove unused match methods from Thing class; enhance test coverage for invalid query strings in test_lankadata.py

--- src/ds/thing/Thing.py
+++ src/ds/thing/Thing.py
@@ -9,13 +9,6 @@ class Thing(ABC):
     def __init__(self, _value):
         object.__setattr__(self, "_value", str(_value))
 
-    @classmethod
-    def is_class_match(cls, query_str):
-        return query_str == cls.__name__
-
-    def is_match(self, query_str):
-        return query_str == self._value
-
     def to_kvpair(self):
         return f"{self.__class__.__name__}:{self._value}"
 
--- tests/test_lankadata.py
+++ tests/test_lankadata.py
@@ -17,3 +17,11 @@ class TestCase(unittest.TestCase):
 
             ds3 = Datumset.from_str(ds2.to_str())
             self.assertEqual(ds1, ds3)
+
+    def test_invalid(self):
+        for query_str in [
+            "Person/Time*GND*Religion/Count",
+            "Person/Time*DSD*Ethnicity1/Count",
+        ]:
+            with self.assertRaises(ValueError):
+                LankaData[query_str]
