e630d27cd30d29d3c8e5880f9f9321ea1703dedb
nuuuwan <nuuuwan@gmail.com>
2026-07-17 13:17:46 +0530

    Add profiling to LankaData queries for performance analysis

--- tests/profile_lankadata.py
+++ tests/profile_lankadata.py
@@ -1,6 +1,12 @@
+import cProfile
+import pstats
+
 from ds import LankaData
 
 if __name__ == "__main__":
+    profiler = cProfile.Profile()
+    profiler.enable()
+
     for query_str in [
         "Person/Time*District*Religion/Count",
         "Person/Time*Province*Religion/Count",
@@ -8,3 +14,11 @@ if __name__ == "__main__":
         "Person/Time*PD*Religion/Count",
     ]:
         LankaData[query_str]
+
+    profiler.disable()
+    profile_path = "tests/profile_lankadata.prof"
+    profiler.dump_stats(profile_path)
+
+    p = pstats.Stats(profile_path)
+
+    p.sort_stats("tottime").print_stats(20)
