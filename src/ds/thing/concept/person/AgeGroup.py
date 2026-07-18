c6abeed14ce630a14e1bf394981ed37ede893fa8
nuuuwan <nuuuwan@gmail.com>
2026-07-18 17:16:24 +0530

    Refactor AgeGroup class and remove unused age group classes

--- src/ds/thing/concept/person/AgeGroup.py
+++ src/ds/thing/concept/person/AgeGroup.py
@@ -1,18 +1,22 @@
-from ds.thing.concept.CategoryConcept import CategoryConcept
+from dataclasses import dataclass
+
+from ds.thing.concept.Concept import Concept
 
 
-class AgeGroup(CategoryConcept):
+@dataclass(frozen=True)
+class AgeGroup(Concept):
+
+    def __init__(self, min_val: int, max_val: int):
+        object.__setattr__(self, "_value", str([min_val, max_val]))
+        object.__setattr__(self, "min_val", min_val)
+        object.__setattr__(self, "max_val", max_val)
+
     @classmethod
-    def list(cls):
-        return [
-            cls("LessThan10"),
-            cls("1019"),
-            cls("2029"),
-            cls("3039"),
-            cls("4049"),
-            cls("5059"),
-            cls("6069"),
-            cls("7079"),
-            cls("8089"),
-            cls("90AndAbove"),
-        ]
+    def from_value(cls, value):
+        value = value.replace("~", "-")
+        value = value.replace(" ", "")
+        parts = value.split("-")
+        min_val, max_val = parts
+        min_val = int(min_val)
+        max_val = int(max_val)
+        return cls(min_val, max_val)
