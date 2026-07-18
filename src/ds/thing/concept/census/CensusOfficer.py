8767f5ee2f5c73cd9d74449f2a88b067d0a7295f
nuuuwan <nuuuwan@gmail.com>
2026-07-18 08:51:17 +0530

    Add CensusOfficer and Census classes with initial structure

---
+++ src/ds/thing/concept/census/CensusOfficer.py
@@ -0,0 +1,18 @@
+# 🤖 via BuildCategortConceptClass.py
+from ds.thing.concept.CategoryConcept import CategoryConcept
+
+
+class CensusOfficer(CategoryConcept):
+    @classmethod
+    def list(cls):
+        return [
+            cls("DeputyCensusCommissioners"),
+            cls("AssistantCensusCommissioners"),
+            cls("TechnicalStaffZonalSupervisorsAndDistrictStatisticalBranchHead"),
+            cls("TechnicalStaffDivisionalCensusOfficer"),
+            cls("TechnicalStaffAreaSupervisors"),
+            cls("TechnicalStaffCircleOfficers"),
+            cls("OtherNonTechnicalStaff"),
+            cls("EnumeratorsWhoUsedTabletComputersCapi"),
+            cls("EnumeratorsWhoUsedSmartPhonesByoad"),
+        ]
