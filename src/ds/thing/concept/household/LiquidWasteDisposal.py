b2eaeeb959f5c55440d8394b6ca51a86dd144ff4
nuuuwan <nuuuwan@gmail.com>
2026-07-20 16:16:49 +0530

    Add liquid waste disposal options to the list in LiquidWasteDisposal class

--- src/ds/thing/concept/household/LiquidWasteDisposal.py
+++ src/ds/thing/concept/household/LiquidWasteDisposal.py
@@ -4,4 +4,12 @@ from ds.thing.concept.CategoryConcept import CategoryConcept
 class LiquidWasteDisposal(CategoryConcept):
     @classmethod
     def list(cls):
-        return []
+        return [
+            cls("to_a_properly_closed_pit"),
+            cls("open_pit"),
+            cls("within_the_premises"),
+            cls("connected_to_a_piped_sewer"),
+            cls("to_a_stream_or_spring_or_river_or_sea"),
+            cls("to_a_drain_on_road"),
+            cls("other"),
+        ]
