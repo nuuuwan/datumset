add23a89787d1bc1dc6f5fb0248bd01f74e8e06a
nuuuwan <nuuuwan@gmail.com>
2026-07-20 16:21:46 +0530

    Implement valid_values method in HouseholdAppliances class to return a list of appliance types

--- src/ds/thing/concept/household/HouseholdAppliances.py
+++ src/ds/thing/concept/household/HouseholdAppliances.py
@@ -2,6 +2,25 @@ from ds.thing.concept.CategoryConcept import CategoryConcept
 
 
 class HouseholdAppliances(CategoryConcept):
+
+    @classmethod
+    def valid_values(cls):
+        return [
+            "radio",
+            "television",
+            "fixed_line_telephone",
+            "smart_mobile_phone",
+            "normal_mobile_phone",
+            "desktop_computer",
+            "laptop_computer",
+            "tablet_computer",
+            "internet_facilities",
+            "bicycle",
+            "motorcycle_or_scooter",
+            "three_wheeler",
+            "other",
+        ]
+
     @classmethod
     def list(cls):
-        return []
+        return [cls(value) for value in cls.valid_values()]
