0c075384492f72b127c0c6fc944d7dc58b17182a
nuuuwan <nuuuwan@gmail.com>
2026-07-20 16:14:28 +0530

    Refactor import statements and update household concepts in ThingFactoryEntityClassListMixin

--- src/ds/thing/ThingFactoryEntityClassListMixin.py
+++ src/ds/thing/ThingFactoryEntityClassListMixin.py
@@ -1,80 +1,72 @@
+from ds.thing.concept.government.AdministrativeEntity import AdministrativeEntity
+from ds.thing.concept.person.AgeGroup import AgeGroup
+from ds.thing.concept.person.AgeGroupWorking import AgeGroupWorking
 from ds.thing.concept.atom.Bool import Bool
-from ds.thing.concept.atom.Float import Float
-from ds.thing.concept.atom.Int import Int
-from ds.thing.concept.atom.Percent import Percent
+from ds.thing.entity.Census import Census
 from ds.thing.concept.census.CensusOfficer import CensusOfficer
 from ds.thing.concept.census.CensusTopic import CensusTopic
-from ds.thing.concept.election.ElectionType import ElectionType
-from ds.thing.concept.election.Party import Party
-from ds.thing.concept.election.Summary import Summary
-from ds.thing.concept.government.AdministrativeEntity import \
-    AdministrativeEntity
-from ds.thing.concept.government.Sector import Sector
 from ds.thing.concept.household.CookingFuel import CookingFuel
+from ds.thing.concept.region.Country import Country
+from ds.thing.concept.region.DSD import DSD
+from ds.thing.concept.person.DisabilityTypes import DisabilityTypes
+from ds.thing.concept.region.District import District
+from ds.thing.concept.region.ED import ED
+from ds.thing.concept.person.EconomicInactivityReason import EconomicInactivityReason
+from ds.thing.concept.person.EducationActivity import EducationActivity
+from ds.thing.concept.election.ElectionType import ElectionType
+from ds.thing.concept.person.EmmigrationReason import EmmigrationReason
+from ds.thing.concept.person.EmploymentStatus import EmploymentStatus
+from ds.thing.concept.person.Ethnicity import Ethnicity
+from ds.thing.concept.atom.Float import Float
 from ds.thing.concept.household.FloorType import FloorType
+from ds.thing.concept.region.GND import GND
+from ds.thing.concept.person.Gender import Gender
+from ds.thing.concept.person.HighestEducationLevel import HighestEducationLevel
+from ds.thing.concept.person.HighestEducationLevel2 import HighestEducationLevel2
+from ds.thing.concept.person.HighestEducationLevel3 import HighestEducationLevel3
+from ds.thing.entity.House import House
 from ds.thing.concept.household.HouseholdOccupancy import HouseholdOccupancy
 from ds.thing.concept.household.HouseholdSize import HouseholdSize
 from ds.thing.concept.household.HouseholdStructure import HouseholdStructure
 from ds.thing.concept.household.HouseholdType import HouseholdType
-from ds.thing.concept.household.Lighting import Lighting
-from ds.thing.concept.household.LivingQuarters import LivingQuarters
-from ds.thing.concept.household.OccupationStatus import OccupationStatus
-from ds.thing.concept.household.OneRoomOrMore import OneRoomOrMore
-from ds.thing.concept.household.OwnershipStatus import OwnershipStatus
-from ds.thing.concept.household.RoofType import RoofType
-from ds.thing.concept.household.SolidWasteDisposal import SolidWasteDisposal
-from ds.thing.concept.household.SourceOfDrinkingWater import \
-    SourceOfDrinkingWater
-from ds.thing.concept.household.ToiletFacilities import ToiletFacilities
-from ds.thing.concept.household.TypeOfUnit import TypeOfUnit
-from ds.thing.concept.household.WallType import WallType
-from ds.thing.concept.household.WaterSupplyAvailability import \
-    WaterSupplyAvailability
-from ds.thing.concept.person.AgeGroup import AgeGroup
-from ds.thing.concept.person.AgeGroupWorking import AgeGroupWorking
-from ds.thing.concept.person.DisabilityTypes import DisabilityTypes
-from ds.thing.concept.person.EconomicInactivityReason import \
-    EconomicInactivityReason
-from ds.thing.concept.person.EducationActivity import EducationActivity
-from ds.thing.concept.person.EmmigrationReason import EmmigrationReason
-from ds.thing.concept.person.EmploymentStatus import EmploymentStatus
-from ds.thing.concept.person.Ethnicity import Ethnicity
-from ds.thing.concept.person.Gender import Gender
-from ds.thing.concept.person.HighestEducationLevel import HighestEducationLevel
-from ds.thing.concept.person.HighestEducationLevel2 import \
-    HighestEducationLevel2
-from ds.thing.concept.person.HighestEducationLevel3 import \
-    HighestEducationLevel3
+from ds.thing.concept.atom.Int import Int
 from ds.thing.concept.person.IsEconomicallyActive import IsEconomicallyActive
+from ds.thing.concept.region.LG import LG
 from ds.thing.concept.person.LanguageLiteracy import LanguageLiteracy
+from ds.thing.concept.household.Lighting import Lighting
+from ds.thing.concept.household.LiquidWasteDisposal import LiquidWasteDisposal
 from ds.thing.concept.person.LiveBirths import LiveBirths
+from ds.thing.concept.household.LivingQuarters import LivingQuarters
 from ds.thing.concept.person.MaritalStatus import MaritalStatus
 from ds.thing.concept.person.MigrationDirection import MigrationDirection
 from ds.thing.concept.person.MigrationReason import MigrationReason
 from ds.thing.concept.person.MigrationStatus import MigrationStatus
-from ds.thing.concept.person.NonCommunicableDisease import \
-    NonCommunicableDisease
-from ds.thing.concept.person.Religion import Religion
-from ds.thing.concept.person.ResidentRelativeToDistrict import \
-    ResidentRelativeToDistrict
-from ds.thing.concept.person.Sex import Sex
-from ds.thing.concept.person.SingleOrMultipleDisabilities import \
-    SingleOrMultipleDisabilities
-from ds.thing.concept.region.Country import Country
-from ds.thing.concept.region.District import District
-from ds.thing.concept.region.DSD import DSD
-from ds.thing.concept.region.ED import ED
-from ds.thing.concept.region.GND import GND
-from ds.thing.concept.region.LG import LG
+from ds.thing.concept.person.NonCommunicableDisease import NonCommunicableDisease
+from ds.thing.concept.household.OccupationStatus import OccupationStatus
+from ds.thing.concept.household.OneRoomOrMore import OneRoomOrMore
+from ds.thing.concept.household.OwnershipStatus import OwnershipStatus
 from ds.thing.concept.region.PD import PD
-from ds.thing.concept.region.Province import Province
-from ds.thing.concept.Time import Time
-from ds.thing.concept.time.TimeGroup0510More import TimeGroup0510More
-from ds.thing.concept.TimeDurationGroup import TimeDurationGroup
-from ds.thing.entity.Census import Census
-from ds.thing.entity.House import House
+from ds.thing.concept.election.Party import Party
+from ds.thing.concept.atom.Percent import Percent
 from ds.thing.entity.Person import Person
+from ds.thing.concept.region.Province import Province
+from ds.thing.concept.person.Religion import Religion
+from ds.thing.concept.person.ResidentRelativeToDistrict import ResidentRelativeToDistrict
+from ds.thing.concept.household.RoofType import RoofType
+from ds.thing.concept.government.Sector import Sector
+from ds.thing.concept.person.Sex import Sex
+from ds.thing.concept.person.SingleOrMultipleDisabilities import SingleOrMultipleDisabilities
+from ds.thing.concept.household.SolidWasteDisposal import SolidWasteDisposal
+from ds.thing.concept.household.SourceOfDrinkingWater import SourceOfDrinkingWater
+from ds.thing.concept.election.Summary import Summary
+from ds.thing.concept.Time import Time
+from ds.thing.concept.TimeDurationGroup import TimeDurationGroup
+from ds.thing.concept.time.TimeGroup0510More import TimeGroup0510More
+from ds.thing.concept.household.ToiletFacilities import ToiletFacilities
+from ds.thing.concept.household.TypeOfUnit import TypeOfUnit
 from ds.thing.entity.Vote import Vote
+from ds.thing.concept.household.WallType import WallType
+from ds.thing.concept.household.WaterSupplyAvailability import WaterSupplyAvailability
 
 
 class ThingFactoryEntityClassListMixin:
@@ -115,7 +107,7 @@ class ThingFactoryEntityClassListMixin:
         AdministrativeEntity,
         Sector,
         # --------------------
-        # household (18)
+        # household (19)
         # --------------------
         CookingFuel,
         FloorType,
@@ -125,16 +117,17 @@ class ThingFactoryEntityClassListMixin:
         #
         HouseholdType,
         Lighting,
+        LiquidWasteDisposal,
         LivingQuarters,
         OccupationStatus,
-        OneRoomOrMore,
         #
+        OneRoomOrMore,
         OwnershipStatus,
         RoofType,
         SolidWasteDisposal,
         SourceOfDrinkingWater,
-        ToiletFacilities,
         #
+        ToiletFacilities,
         TypeOfUnit,
         WallType,
         WaterSupplyAvailability,
