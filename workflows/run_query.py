325ae2c6191186b488463c436abaf8576d147db0
nuuuwan <nuuuwan@gmail.com>
2026-07-25 09:47:20 +0530

    Add run_query script to execute queries and log results

---
+++ workflows/run_query.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+from utils_future import Log
+
+from ds import LankaData
+
+log = Log("run_query")
+
+if __name__ == "__main__":
+    query_str = sys.argv[1]
+    log.debug(f"{query_str=}")
+    ds = LankaData[query_str]
+    print(json.dumps(ds.to_data(), indent=2))
