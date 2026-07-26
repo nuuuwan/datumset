e704f506bb5ff281757c1e346bd0b5e0f3613383
nuuuwan <nuuuwan@gmail.com>
2026-07-26 14:39:27 +0530

    Add VisualFactory class to manage visual components and their retrieval

---
+++ src/ds/visual/VisualFactory.py
@@ -0,0 +1,18 @@
+from ds.visual.BarChart import BarChart
+from ds.visual.MapVisual import MapVisual
+from ds.visual.PieChart import PieChart
+from ds.visual.StackedBarChart import StackedBarChart
+
+
+class VisualFactory:
+
+    @classmethod
+    def visual_class_list(cls):
+        return [BarChart, MapVisual, PieChart, StackedBarChart]
+
+    @classmethod
+    def __class_getitem__(cls, visual_class_name):
+        for visual_class in cls.visual_class_list():
+            if visual_class.__name__ == visual_class_name:
+                return visual_class
+        raise ValueError(f"Visual class '{visual_class_name}' not found")
