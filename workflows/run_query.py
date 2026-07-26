d6f770f0015c9958caf9e7005cd4cc852b850529
nuuuwan <nuuuwan@gmail.com>
2026-07-26 10:54:17 +0530

    Refactor query string assignment in run_query.py for improved readability and default handling

--- workflows/run_query.py
+++ workflows/run_query.py
@@ -8,7 +8,11 @@ from ds import LankaData
 log = Log("run_query")
 
 if __name__ == "__main__":
-    query_str = sys.argv[1]
+    query_str = (
+        sys.argv[1]
+        if len(sys.argv) > 1
+        else "Person/Time*Country*Religion/Count"
+    )
     log.debug(f"{query_str=}")
     ds = LankaData[query_str]
     print(json.dumps(ds.to_data(), indent=2))
