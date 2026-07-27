59ec04957ddd54d5a80590dcdd03589b5aee8874
nuuuwan <nuuuwan@gmail.com>
2026-07-27 10:35:28 +0530

    Implement split method in DatumsetSplitMixin and add unit tests for Datumset

--- src/ds/datumset/Datumset.py
+++ src/ds/datumset/Datumset.py
@@ -2,8 +2,29 @@ from ds.datumset.DatumsetBase import DatumsetBase
 from ds.datumset.DatumsetSerializeMixin import DatumsetSerializeMixin
 
 
+class DatumsetSplitMixin:
+
+    def split(self, *split_dims: list[str]) -> list["Datumset"]:
+        idx = {}
+        for datum in self:
+            key_items = []
+            for split_dim in split_dims:
+                split_dim_value = datum.dim_idx[split_dim]
+                key_items.append(f"{split_dim}:{split_dim_value}")
+            key = "/".join(key_items)
+            if key not in idx:
+                idx[key] = []
+            idx[key].append(datum)
+
+        datumset_list = []
+        for datum_list_for_key in idx.values():
+            datumset_list.append(Datumset(*datum_list_for_key))
+        return datumset_list
+
+
 class Datumset(
     DatumsetSerializeMixin,
     DatumsetBase,
+    DatumsetSplitMixin,
 ):
     pass
---
+++ tests/test_datumset.py
@@ -0,0 +1,106 @@
+import unittest
+
+from ds import Datumset
+
+TEST_DATUMSET = Datumset.from_data(
+    {
+        "Person": {
+            "Time:2012": {
+                "District:LK-11": {
+                    "Religion:buddhist": {
+                        "Count": "Int:123",
+                    },
+                    "Religion:hindu": {
+                        "Count": "Int:456",
+                    },
+                },
+                "District:LK-12": {
+                    "Religion:buddhist": {
+                        "Count": "Int:1231",
+                    },
+                    "Religion:hindu": {
+                        "Count": "Int:4561",
+                    },
+                },
+            },
+            "Time:2024": {
+                "District:LK-11": {
+                    "Religion:buddhist": {
+                        "Count": "Int:1230",
+                    },
+                    "Religion:hindu": {
+                        "Count": "Int:4560",
+                    },
+                },
+                "District:LK-12": {
+                    "Religion:buddhist": {
+                        "Count": "Int:12310",
+                    },
+                    "Religion:hindu": {
+                        "Count": "Int:45610",
+                    },
+                },
+            },
+        }
+    },
+)
+
+
+class TestCase(unittest.TestCase):
+    def test_split(self):
+        datumset = TEST_DATUMSET
+
+        split_datumsets = datumset.split("Time")
+
+        self.assertEqual(len(split_datumsets), 2)
+
+        self.assertEqual(
+            split_datumsets[0].to_data(),
+            {
+                "Person": {
+                    "Time:2012": {
+                        "District:LK-11": {
+                            "Religion:buddhist": {
+                                "Count": "Int:123",
+                            },
+                            "Religion:hindu": {
+                                "Count": "Int:456",
+                            },
+                        },
+                        "District:LK-12": {
+                            "Religion:buddhist": {
+                                "Count": "Int:1231",
+                            },
+                            "Religion:hindu": {
+                                "Count": "Int:4561",
+                            },
+                        },
+                    },
+                }
+            },
+        )
+        self.assertEqual(
+            split_datumsets[1].to_data(),
+            {
+                "Person": {
+                    "Time:2024": {
+                        "District:LK-11": {
+                            "Religion:buddhist": {
+                                "Count": "Int:1230",
+                            },
+                            "Religion:hindu": {
+                                "Count": "Int:4560",
+                            },
+                        },
+                        "District:LK-12": {
+                            "Religion:buddhist": {
+                                "Count": "Int:12310",
+                            },
+                            "Religion:hindu": {
+                                "Count": "Int:45610",
+                            },
+                        },
+                    },
+                }
+            },
+        )
