b7228807203d1891b0ea526a4f7662447f996533
nuuuwan <nuuuwan@gmail.com>
2026-07-29 16:03:33 +0530

    Refactor: improve import formatting and update x_dim_key and stack_dim_key assignments for clarity

--- src/ds/visual/stacked_bar_chart/StackedBarChart.py
+++ src/ds/visual/stacked_bar_chart/StackedBarChart.py
@@ -1,9 +1,12 @@
-from ds.visual.stacked_bar_chart.StackedBarChartColorMixin import \
-    StackedBarChartColorMixin
-from ds.visual.stacked_bar_chart.StackedBarChartDataMixin import \
-    StackedBarChartDataMixin
-from ds.visual.stacked_bar_chart.StackedBarChartPlotMixin import \
-    StackedBarChartPlotMixin
+from ds.visual.stacked_bar_chart.StackedBarChartColorMixin import (
+    StackedBarChartColorMixin,
+)
+from ds.visual.stacked_bar_chart.StackedBarChartDataMixin import (
+    StackedBarChartDataMixin,
+)
+from ds.visual.stacked_bar_chart.StackedBarChartPlotMixin import (
+    StackedBarChartPlotMixin,
+)
 from ds.visual.visual.Visual import Visual
 
 
@@ -16,8 +19,8 @@ class StackedBarChart(
 
     def __init__(self, datumset):
         super().__init__(datumset)
-        self.x_dim_key = self._get_first_varying_dim_key()
-        self.stack_dim_key = self._get_stack_dim_key(self.x_dim_key)
+        self.x_dim_key = self._get_varying_dim_keys()[-2]
+        self.stack_dim_key = self._get_varying_dim_keys()[-1]
         self.y_cell_key = self._get_y_cell_key()
         self.display_datumsets = self._get_display_datumsets(
             {self.x_dim_key, self.stack_dim_key}
