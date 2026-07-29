26ba9ee6fb3653f5fae2367948b231dc6fac934a
nuuuwan <nuuuwan@gmail.com>
2026-07-29 16:37:03 +0530

    Refactor: remove unnecessary checks in lightness calculation and default color handling

--- src/ds/visual/map_visual/MapVisualColorMixin.py
+++ src/ds/visual/map_visual/MapVisualColorMixin.py
@@ -14,8 +14,6 @@ class MapVisualColorMixin:
         h, l, s = colorsys.rgb_to_hls(*base_rgb)
         light_l = min(0.95, max(0.55, l * 1.2))
         dark_l = max(0.12, min(0.45, l * 0.6))
-        if dark_l >= light_l:
-            dark_l = max(0.0, light_l - 0.25)
         colors = []
         for i in range(256):
             ratio = i / 255
@@ -25,6 +23,4 @@ class MapVisualColorMixin:
 
     def _get_value_cmap(self):
         base_color = self._get_single_fixed_dim_color({self.region_dim_key})
-        if base_color is None:
-            return "YlOrRd"
         return self._build_hsl_lightness_cmap(base_color)
