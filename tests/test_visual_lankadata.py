8d5639e1027cfd59cd85f6ed6ecf9b00ac6813cc
nuuuwan <nuuuwan@gmail.com>
2026-07-31 18:15:33 +0530

    Replace JSON data file with YAML format for test cases and update related code

--- tests/test_visual_lankadata.py
+++ tests/test_visual_lankadata.py
@@ -2,12 +2,12 @@ import os
 import re
 import unittest
 
-from utils_future import JSONFile
+from utils_future import YAMLFile
 
 from ds import VisualLankaData
 
 DATA_PATH = os.path.join(
-    os.path.dirname(__file__), "test_visual_lankadata.data.json"
+    os.path.dirname(__file__), "test_visual_lankadata.data.yaml"
 )
 
 
@@ -15,7 +15,7 @@ class TestCase(unittest.TestCase):
 
     @staticmethod
     def get_query_strs():
-        query_strs_file = JSONFile(DATA_PATH)
+        query_strs_file = YAMLFile(DATA_PATH)
         query_strs = query_strs_file.read()
         for query_str in query_strs:
             tokens = query_str.split("/")
@@ -23,7 +23,15 @@ class TestCase(unittest.TestCase):
                 raise ValueError(f"Invalid query string: {query_str}")
         query_strs = list(set(query_strs))
         query_strs.sort()
-        query_strs_file.write(query_strs)
+        lines = []
+        prev_first_token = None
+        for query_str in query_strs:
+            first_token = query_str.split("/")[0]
+            if first_token != prev_first_token:
+                lines.extend([f"# {prev_first_token}"])
+            lines.append(f"- {query_str}")
+            prev_first_token = first_token
+        query_strs_file.write_lines(lines)
         return query_strs
 
     @staticmethod
