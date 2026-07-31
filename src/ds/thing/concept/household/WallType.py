8c489cd9ee78190db6e7f24b1aa3ebbe9ed4c348
nuuuwan <nuuuwan@gmail.com>
2026-07-31 10:13:51 +0530

    Refactor code structure for improved readability and maintainability

--- src/ds/thing/concept/household/WallType.py
+++ src/ds/thing/concept/household/WallType.py
@@ -6,61 +6,61 @@ class WallType(CategoryConcept):
     @classmethod
     def valid_values(cls):
         return [
-            'bricks',
-            'cabook',
-            'cadjan_palmyrah',
-            'cement_block',
-            'cement_block_stone',
-            'granite_cube_stones',
-            'not_relevant',
-            'other',
-            'plank_or_metal_sheet',
-            'planks_metal',
-            'pressed_soil_bricks',
-            'warichchi_mud',
-            'zink_aluminium',
+            "bricks",
+            "cabook",
+            "cadjan_palmyrah",
+            "cement_block",
+            "cement_block_stone",
+            "granite_cube_stones",
+            "not_relevant",
+            "other",
+            "plank_or_metal_sheet",
+            "planks_metal",
+            "pressed_soil_bricks",
+            "warichchi_mud",
+            "zink_aluminium",
         ]
 
     @classmethod
     def map_alias(cls):
         return {
-            'bricks': [
-                'brick',
+            "bricks": [
+                "brick",
             ],
-            'cadjan_palmyrah': [
-                'cadjan_or_palmyrah',
+            "cadjan_palmyrah": [
+                "cadjan_or_palmyrah",
             ],
-            'cement_block_stone': [
-                'cement_block_or_stone',
+            "cement_block_stone": [
+                "cement_block_or_stone",
             ],
-            'planks_metal': [
-                'planks_metal_sheets_asbestos',
+            "planks_metal": [
+                "planks_metal_sheets_asbestos",
             ],
-            'pressed_soil_bricks': [
-                'soil_bricks',
+            "pressed_soil_bricks": [
+                "soil_bricks",
             ],
-            'warichchi_mud': [
-                'mud',
+            "warichchi_mud": [
+                "mud",
             ],
-            'zink_aluminium': [
-                'zink_aluminium_sheets',
+            "zink_aluminium": [
+                "zink_aluminium_sheets",
             ],
         }
 
     @classmethod
     def get_color_map(cls):
         return {
-            'bricks': '#D05D38',
-            'cabook': '#6CD038',
-            'cadjan_palmyrah': '#D0AF38',
-            'cement_block': '#38D056',
-            'cement_block_stone': '#3840D0',
-            'granite_cube_stones': '#D03847',
-            'not_relevant': '#cccccc',
-            'other': '#cccccc',
-            'plank_or_metal_sheet': '#8238D0',
-            'planks_metal': '#3873D0',
-            'pressed_soil_bricks': '#D03899',
-            'warichchi_mud': '#38C5D0',
-            'zink_aluminium': '#9FD038',
+            "bricks": "#cc0000",
+            "cabook": "#ff8800",
+            "cadjan_palmyrah": "#D0AF38",
+            "cement_block": "#cccccc",
+            "cement_block_stone": "#888888",
+            "granite_cube_stones": "#000000",
+            "not_relevant": "#cccccc",
+            "other": "#cccccc",
+            "plank_or_metal_sheet": "#8238D0",
+            "planks_metal": "#3873D0",
+            "pressed_soil_bricks": "#D03899",
+            "warichchi_mud": "#cc8800",
+            "zink_aluminium": "#38C5D0",
         }
